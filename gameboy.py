9497dbbbc2870aff148d42bd681296b358213d1b
superguy2876 <patton_ist07@hotmail.com>
2021-07-15 02:47:07 +0900

    Minor update

--- gameboy.py
+++ gameboy.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 from pyboy import PyBoy, WindowEvent, plugins
-import gamewrapper_pkmnred
+
 
 
 pyboy = PyBoy('pkmnred.gb')
@@ -15,10 +15,8 @@ pyboy = PyBoy('pkmnred.gb')
 #     pass
 # except KeyboardInterrupt as e:
 #   print('BREAK!')
-print(pyboy.__slots__)
-print('\n'.join(dir(pyboy)))
 
-red = gamewrapper_pkmnred.GameWrapperPokemonRed(pyboy, pyboy.mb, sys.argv)
+
 
 # pyboy = PyBoy('pkmnred.gb')
 # print(pyboy.cartridge_title())
